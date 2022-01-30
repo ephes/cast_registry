e30d52c6e20359c80aa5e2a47d75ddde08df676c
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-01-30 07:34:20 +0100

    remove obsolete views

--- apps/registry/views.py
+++ apps/registry/views.py
@@ -29,60 +29,6 @@ def fade_out(request: HttpRequest) -> HttpResponse:
     return HttpResponse(status=200, content="")
 
 
-@require_GET
-def csrf_demo(request: HttpRequest) -> HttpResponse:
-    return render(request, "csrf-demo.html")
-
-
-@require_http_methods(("POST",))
-def csrf_demo_checker(request: HttpRequest) -> HttpResponse:
-    form = OddNumberForm(request.POST)
-    if form.is_valid():
-        number = form.cleaned_data["number"]
-        number_is_odd = number % 2 == 1
-    else:
-        number_is_odd = False
-    return render(
-        request,
-        "csrf-demo-checker.html",
-        {"form": form, "number_is_odd": number_is_odd},
-    )
-
-
-@dataclass
-class Person:
-    id: int
-    name: str
-
-
-faker = Faker()
-people = [Person(id=i, name=faker.name()) for i in range(1, 235)]
-
-
-@require_GET
-def partial_rendering(request: HttpRequest) -> HttpResponse:
-    # Standard Django pagination
-    page_num = request.GET.get("page", "1")
-    page = Paginator(object_list=people, per_page=10).get_page(page_num)
-
-    # The htmx magic - use a different, minimal base template for htmx
-    # requests, allowing us to skip rendering the unchanging parts of the
-    # template.
-    if request.htmx:
-        base_template = "_partial.html"
-    else:
-        base_template = "_base.html"
-
-    return render(
-        request,
-        "partial-rendering.html",
-        {
-            "base_template": base_template,
-            "page": page,
-        },
-    )
-
-
 deployment_in_progress = False
 
 
