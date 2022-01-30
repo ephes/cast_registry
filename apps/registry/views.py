dd2039bfbdd4dd87b9cc7e074eeeb881dfd0b983
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-01-30 16:20:28 +0100

    removed obsolete stuff

--- apps/registry/views.py
+++ apps/registry/views.py
@@ -1,11 +1,10 @@
 from django.contrib import messages
-from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_GET, require_POST
+from django.views.decorators.http import require_GET
 from django_htmx.http import HTMX_STOP_POLLING
 
 from .deployment import Client
@@ -70,55 +69,3 @@ def deploy_state(request: HttpRequest, domain_id: int, deployment_id: int) -> Ht
 @csrf_exempt
 def fade_out(request: HttpRequest) -> HttpResponse:
     return HttpResponse(status=200, content="")
-
-
-deployment_in_progress = False
-
-
-@require_POST
-def create_domain(request: HttpRequest) -> HttpResponse:
-    form = DomainForm(request.POST)
-    if form.is_valid():
-        domain = form.save(commit=False)
-        user = get_user_model().objects.get(username="jochen")
-        domain.owner = user
-        domain.save()
-        global deployment_in_progress
-        deployment_in_progress = True
-        return redirect("list_domains")
-    else:
-        return redirect("list_domains")
-
-
-state = 0
-
-
-@require_GET
-def list_domains(request: HttpRequest) -> HttpResponse:
-    print("get data: ", request.GET)
-    global state
-    state = 0
-    domains = Domain.objects.all()
-    form = DomainForm()
-    return render(
-        request,
-        "registry/list-domains.html",
-        {"domains": domains, "form": form, "show_progress": deployment_in_progress},
-    )
-
-
-@require_GET
-def show_messages(request: HttpRequest) -> HttpResponse:
-    global state
-    body = f"""
-    hello world {state}
-    """
-    code = 200
-    if state <= 10:
-        content = body
-    else:
-        content = "stop"
-        code = 286
-    state += 1
-    print("content: ", content)
-    return HttpResponse(status=code, content=content)
--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from apps.registry.views import (
-    create_domain,
-    deploy_progress,
-    fade_out,
-    home,
-    list_domains,
-    register,
-    show_messages,
-    deploy_state,
-)
+from apps.registry.views import deploy_progress, deploy_state, fade_out, home, register
 
 urlpatterns = [
-    path("", home),
     path("admin/", admin.site.urls),
-    path("domain/create/", create_domain, name="create_domain"),
-    path("domain/", list_domains, name="list_domains"),
-    path("messages/", show_messages, name="messages"),
+    path("", home),
     path("register/", register, name="register"),
     path("fade_out/", fade_out, name="fade_out"),
     path("deploy-progress/<int:domain_id>/<int:deployment_id>", deploy_progress, name="deploy_progress"),
