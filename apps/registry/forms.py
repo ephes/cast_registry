c8e89bb7be9ca42b06c440ec8d00dfb1c1a8c46d
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-01-30 07:34:10 +0100

    remove obsolete form

--- apps/registry/forms.py
+++ apps/registry/forms.py
@@ -3,10 +3,6 @@ from django import forms
 from .models import Domain
 
 
-class OddNumberForm(forms.Form):
-    number = forms.IntegerField()
-
-
 class DomainForm(forms.ModelForm):
     class Meta:
         fields = ["fqdn"]
