13fda8e01555c6e558bbbd336d5aef2361fc5ee3
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-07-04 07:36:43 +0200

    #3 added login required tests for register view

--- apps/registry/tests/views_test.py
+++ apps/registry/tests/views_test.py
@@ -8,3 +8,20 @@ def test_register_not_authenticated(client):
     r = client.get(url)
     assert r.status_code == 302
     assert "login" in r.url
+
+
+@pytest.fixture
+def user(django_user_model):
+    username, password = "user1", "password"
+    user = django_user_model.objects.create_user(username=username, password=password)
+    user._password = password
+    return user
+
+
+@pytest.mark.django_db
+def test_register_authenticated(client, user):
+    client.login(username=user.username, password=user._password)
+    url = reverse("register")
+    r = client.get(url)
+    assert r.status_code == 200
+    assert "form action" in r.content.decode("utf8")
