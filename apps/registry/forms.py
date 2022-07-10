1b58055ec609693cd16ec9055ce20055fc503001
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-07-10 15:34:40 +0200

    #18 tried to make fqdn field bigger -> fail

--- apps/registry/forms.py
+++ apps/registry/forms.py
@@ -4,6 +4,8 @@ from .models import Domain
 
 
 class DomainForm(forms.ModelForm):
+    fqdn = forms.CharField(max_length=255, min_length=2)
+
     class Meta:
         fields = ["fqdn"]
         model = Domain
