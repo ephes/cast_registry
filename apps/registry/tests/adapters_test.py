3fc7cbdfcf042bf74232bd5816508b1726cad151
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-08-02 18:19:36 +0200

    #35 added test for social account adapter allowed registration

--- apps/registry/tests/adapters_test.py
+++ apps/registry/tests/adapters_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..adapters import AccountAdapter
+from ..adapters import AccountAdapter, SocialAccountAdapter
 
 
 @pytest.mark.parametrize(
@@ -13,3 +13,15 @@ from ..adapters import AccountAdapter
 def test_account_adapter_open_for_registration(settings, allow_registration_setting, expected):
     settings.ACCOUNT_ALLOW_REGISTRATION = allow_registration_setting
     assert AccountAdapter().is_open_for_signup(None) == expected
+
+
+@pytest.mark.parametrize(
+    "allow_registration_setting, expected",
+    [
+        (False, False),
+        (True, True),
+    ],
+)
+def test_social_account_adapter_open_for_registration(settings, allow_registration_setting, expected):
+    settings.ACCOUNT_ALLOW_REGISTRATION = allow_registration_setting
+    assert SocialAccountAdapter().is_open_for_signup(None, None) == expected
