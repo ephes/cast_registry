39e1b23a3807a922f88524af7ac11342408918f3
Jochen Wersdörfer <jochen@wersdoerfer.de>
2022-08-02 07:16:11 +0200

    #33 move registry urls into urls.py of registry app

--- apps/registry/urls.py
+++ apps/registry/urls.py
@@ -7,5 +7,9 @@ from . import views  # noqa
 app_name = "registry"
 
 urlpatterns: list[Any] = [
-    # path("example/", views.example, name="example")
+    path("", views.home),
+    path("domains/", views.domains, name="domains"),
+    path("domain-deployments/<int:domain_id>/", views.domain_deployments, name="domain_deployments"),
+    path("fade_out/", views.fade_out, name="fade_out"),
+    path("deploy-state/<int:deployment_id>/", views.deploy_state, name="deploy_state"),
 ]
--- config/urls.py
+++ config/urls.py
@@ -1,24 +1,16 @@
+from typing import Any
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-from apps.registry.views import (
-    deploy_state,
-    domain_deployments,
-    domains,
-    fade_out,
-    home,
-)
+from apps.registry.urls import urlpatterns as registry_urlpatterns
 
-urlpatterns = [
+urlpatterns: list[Any] = [
+    # admin
     path(settings.ADMIN_URL, admin.site.urls),
-    path("", home),
-    path("domains/", domains, name="domains"),
-    path("domain-deployments/<int:domain_id>/", domain_deployments, name="domain_deployments"),
-    path("fade_out/", fade_out, name="fade_out"),
-    path("deploy-state/<int:deployment_id>/", deploy_state, name="deploy_state"),
     # debug toolbar
     path("__debug__/", include("debug_toolbar.urls")),
     # allauth
     path("accounts/", include("allauth.urls")),
-]
+] + registry_urlpatterns
