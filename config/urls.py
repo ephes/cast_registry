01c1ac2017e4eab5091551bd5c21e4fd8dd50857
Jochen Wersdörfer <jochen@wersdoerfer.de>
2024-10-17 12:48:42 +0200

    #43 make debug_toolbar dev only

--- config/urls.py
+++ config/urls.py
@@ -9,8 +9,11 @@ from apps.registry.urls import urlpatterns as registry_urlpatterns
 urlpatterns: list[Any] = [
     # admin
     path(settings.ADMIN_URL, admin.site.urls),
-    # debug toolbar
-    path("__debug__/", include("debug_toolbar.urls")),
     # allauth
     path("accounts/", include("allauth.urls")),
 ] + registry_urlpatterns
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
